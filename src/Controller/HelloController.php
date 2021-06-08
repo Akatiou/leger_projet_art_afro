c3209c4e5fe439baa1d9e0ed78a3e689dfba4024
Sissoko Aïssata <aissata001@hotmail.fr>
2021-06-08 14:13:46 +0200

    navbar + home

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -7,13 +7,20 @@ use App\Taxes\Detector;
 use App\Taxes\Calculator;
 use Cocur\Slugify\Slugify;
 use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class HelloController
+class HelloController extends AbstractController
 {
+    protected $twig;
+
+    // public function __construct(Environment $twig)
+    // {
+    //     $this->twig = $twig;
+    // }
     // protected $calculator;
 
     // public function __construct(Calculator $calculator)
@@ -48,6 +55,53 @@ class HelloController
 
         // dump($tva);
 
-        return new Response("Hello $prenom !");
+        // return new Response("Hello $prenom !");
+
+        $html = $this->render('hello.html.twig', [
+            'prenom' => $prenom,
+            'age' => 28,
+            'prenoms' => [
+                'Jessey',
+                'Aïssata',
+                'Jamaal'
+            ],
+            // Exercice 1
+            'ages' => [
+                12,
+                18,
+                29,
+                15
+            ],
+            // Explication 2
+            'formateur' => [
+                'prenom' => 'Lior',
+                'nom' => 'Chamla',
+                'age' => 33
+            ],
+            // Explication 3 (inclusion de templates)
+            'formateur1' => ['prenom' => 'Lior', 'nom' => 'Chamla'],
+            'formateur2' => ['prenom' => 'Jerôme', 'nom' => 'Dupont'],
+            // Explication 4
+        ]);
+        return $this->render('hello.html.twig', [
+            'prenom' => $prenom
+        ]);
     }
+
+    /**
+     * @Route("/example", name="example")
+     */
+
+    public function example()
+    {
+        return $this->render('example.html.twig', [
+            'age' => 33
+        ]);
+    }
+
+    // protected function render(string $path, array $variables = [])
+    // {
+    //     $html = $this->twig->render($path, $variables);
+    //     return new Response($html);
+    // }
 }
